c37ed82544cef5c96f8d7c2a38b9a32055196010
Antonio Radovcic <mail@niorad.com>
2019-10-04 14:25:36 +0200

    no nodes

---
+++ lambdas/devlids.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+
+const fetch = require("./node-fetch.js");
+const API_ENDPOINT = 'https://devlids.com/api/';
+
+exports.handler = async function(event, context) {
+  let response;
+	let data;
+
+	console.log("Function Debug" );
+	console.log("Secret:", process.env.DEVLIDS_TOKEN );
+	console.log("Path:", event.queryStringParameters.path );
+
+	try {
+		response = await fetch(
+			API_ENDPOINT + event.queryStringParameters.path,
+			{
+				headers: {
+					Authorization: process.env.DEVLIDS_TOKEN
+				}
+			}
+		);
+
+		data = await response.json();
+	} catch (err) {
+		return {
+			statusCode: err.statusCode || 500,
+			body: JSON.stringify({
+				error: err.message
+			})
+		};
+	}
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			data
+		})
+	};
+};
